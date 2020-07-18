2fd7a40d0d67f83ad07587b60b47057e5d4ce32b
Alexsandro G Bezerra <aalexo1313@gmail.com>
2020-07-17 21:58:04 -0300

    feat(server): add typeorm

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,12 @@
+import 'reflect-metadata'
+import { createConnection } from 'typeorm'
+
 import app from './app'
 
 const PORT = process.env.PORT || 3333
 
-app.listen(PORT, () => {
-  console.log('Running on port ' + PORT)
+createConnection().then(() => {
+  app.listen(PORT, () => {
+    console.log('Running on port ' + PORT)
+  })
 })
